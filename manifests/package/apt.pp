5f82024be02e217c4b3479f955e4bc6032af856a
Eric R Frechette <eric.r.frechette@aero.org>
2019-11-01 11:37:55 -0600

    updated apt to use apt.conf.d and include https proxy

--- manifests/package/apt.pp
+++ manifests/package/apt.pp
@@ -2,13 +2,12 @@
 # Uses the puppetlabs-apt module to manage apt package manager proxies
 # https://forge.puppetlabs.com/puppetlabs/apt
 class httpproxy::package::apt {
-
-  class { '::apt':
-    proxy => {
-      'ensure' => $httpproxy::packagemanager::ensure,
-      'host'   => $httpproxy::http_proxy,
-      'port'   => $httpproxy::http_proxy_port,
-    },
+  file { 'apt_via_proxy':
+    path    => '/etc/apt/apt.conf.d/05proxy',
+    ensure  => $httpproxy::packagemanager::ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Acquire::http::Proxy \"${httpproxy::proxy_uri}\";\nAcquire::https::Proxy \"${httpproxy::proxy_uri}\";"
   }
-  contain '::apt'
 }
