e1587abf4818a86288723082b2e85edf7cc35f57
Eric R Frechette <eric.r.frechette@aero.org>
2019-11-01 11:52:04 -0600

    cleaned up apt.pp

--- manifests/package/apt.pp
+++ manifests/package/apt.pp
@@ -2,12 +2,18 @@
 # Uses the puppetlabs-apt module to manage apt package manager proxies
 # https://forge.puppetlabs.com/puppetlabs/apt
 class httpproxy::package::apt {
-  file { 'apt_via_proxy':
+  $lines = [
+    '# File managed by Puppet',
+    '',
+    "Acquire::http::Proxy \"${httpproxy::proxy_uri}\"";
+    "Acquire::https::Proxy \"${httpproxy::proxy_uri}\"";
+  ]
+
+  file { '/etc/apt/apt.conf.d/05proxy':
     ensure  => $httpproxy::packagemanager::ensure,
-    path    => '/etc/apt/apt.conf.d/05proxy',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => "Acquire::http::Proxy \"${httpproxy::proxy_uri}\";\nAcquire::https::Proxy \"${httpproxy::proxy_uri}\";",
+    content => join($lines, "\n"),
   }
 }
