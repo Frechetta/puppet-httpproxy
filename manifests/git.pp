3774cdec7fdcc0615eacf7e089a2af27f2afddc8
Eric R Frechette <eric.r.frechette@aero.org>
2019-12-03 15:41:53 -0700

    added path to exec

--- manifests/git.pp
+++ manifests/git.pp
@@ -7,8 +7,14 @@ class httpproxy::git {
     }
 
     if $httpproxy::git::ensure == 'present' {
-        exec { "$(which git) config --system http.proxy ${httpproxy::proxy_uri}": }
+        exec { 'git-proxy':
+            command => "git config --system http.proxy ${httpproxy::proxy_uri}",
+            path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+        }
     } elsif $httpproxy::git::ensure == 'absent' {
-        exec { '$(which git) config --system --unset http.proxy': }
+        exec { 'git-proxy':
+            command => 'git config --system --unset http.proxy',
+            path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+        }
     }
 }
