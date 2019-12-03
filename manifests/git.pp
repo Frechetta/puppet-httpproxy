7266326d908fefd64444b7cca5aea1d830eff7e2
Eric R Frechette <eric.r.frechette@aero.org>
2019-12-03 15:36:23 -0700

    fixing git exec command

--- manifests/git.pp
+++ manifests/git.pp
@@ -7,8 +7,8 @@ class httpproxy::git {
     }
 
     if $httpproxy::git::ensure == 'present' {
-        exec { "git config --system http.proxy ${httpproxy::proxy_uri}": }
+        exec { "$(which git) config --system http.proxy ${httpproxy::proxy_uri}": }
     } elsif $httpproxy::git::ensure == 'absent' {
-        exec { 'git config --system --unset http.proxy': }
+        exec { '$(which git) config --system --unset http.proxy': }
     }
 }
