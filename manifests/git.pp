ac1b2db38daf2f6c83eefe89f962477c0d366f41
Eric R Frechette <eric.r.frechette@aero.org>
2019-12-04 15:52:14 -0700

    changed to using full path to git in command

--- manifests/git.pp
+++ manifests/git.pp
@@ -8,13 +8,13 @@ class httpproxy::git {
 
     if $httpproxy::git::ensure == 'present' {
         exec { 'git-proxy':
-            command => "git config --system http.proxy ${httpproxy::proxy_uri}",
-            path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+            command => "/usr/bin/git config --system http.proxy ${httpproxy::proxy_uri}",
+            # path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
         }
     } elsif $httpproxy::git::ensure == 'absent' {
         exec { 'git-proxy':
-            command => 'git config --system --unset http.proxy',
-            path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
+            command => '/usr/bin/git config --system --unset http.proxy',
+            # path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
         }
     }
 }
