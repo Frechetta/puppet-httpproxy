2fc851bbb34119118f994c34fedee8cda17bbe49
Eric R Frechette <eric.r.frechette@aero.org>
2019-12-10 17:18:23 -0700

    added 'unless' attributes

--- manifests/git.pp
+++ manifests/git.pp
@@ -27,11 +27,13 @@ class httpproxy::git {
     if $httpproxy::git::ensure == 'present' {
         exec { 'git-proxy':
             command => "/usr/bin/git config --system http.proxy ${httpproxy::proxy_uri}",
+            unless  => "[[ \"$(git config --system --get http.proxy)\" == \"${httpproxy::proxy_uri}\" ]]",
             # path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
         }
     } elsif $httpproxy::git::ensure == 'absent' {
         exec { 'git-proxy':
             command => '/usr/bin/git config --system --unset http.proxy',
+            unless  => '[[ -z "$(git config --system --get http.proxy)" ]]'
             # path    => [ '/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin' ],
         }
     }
